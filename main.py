6323a261f35bbd140819e44665b1e39e394451f3
Gabriel <brunengo.gabriel@gmail.com>
2023-03-14 04:23:21 -0300

    update

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import bs4
+from pathlib import Path
+
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0'}
+link = "https://es.wikipedia.org/wiki/Primera_Division_de_Argentina"
+res = requests.get(link,headers=headers)
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+data = soup.select("#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(21) > td > a").text
+contenido = "{campeon:"+ data +"}"
+ruta = Path(__file__).parent.resolve().joinpath('datos.json')
+archivo = open(ruta,'w',encoding='utf-8')
+archivo.write()
+archivo.close()
+
+
+
+
+
+
