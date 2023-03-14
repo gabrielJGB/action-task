5ae981c183d12f45a4a05acc564af02f3edb1f2b
Gabriel <brunengo.gabriel@gmail.com>
2023-03-14 13:50:44 -0300

    update

--- main.py
+++ main.py
@@ -7,14 +7,9 @@ link = "https://es.wikipedia.org/wiki/Primera_Division_de_Argentina"
 res = requests.get(link,headers=headers)
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
 data = soup.select("#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(21) > td > a")[0].text
+
 contenido = "{\"campeon\":\" %s \"}" %(data)
 ruta = Path(__file__).parent.resolve().joinpath('datos.json')
 archivo = open(ruta,'w',encoding='utf-8')
 archivo.write(contenido)
 archivo.close()
-
-
-
-
-
-
