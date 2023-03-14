2cda93b22cf5b2462d519ffc77872391584fc843
Gabriel <brunengo.gabriel@gmail.com>
2023-03-14 06:01:15 -0300

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Geck
 link = "https://es.wikipedia.org/wiki/Primera_Division_de_Argentina"
 res = requests.get(link,headers=headers)
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-data = soup.select("#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(21) > td > a").text
+data = soup.select("#mw-content-text > div.mw-parser-output > table.infobox > tbody > tr:nth-child(21) > td > a")[0].text
 contenido = "{campeon:"+ data +"}"
 ruta = Path(__file__).parent.resolve().joinpath('datos.json')
 archivo = open(ruta,'w',encoding='utf-8')
